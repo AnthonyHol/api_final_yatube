4887a3819d857b3e2ac0765ba058ad8a47c4bc2b
Холкин Антон <1tonyhol1@gmail.com>
2022-08-28 10:31:35 +0300

    the first commit

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,74 @@
+from posts.models import Comment, Follow, Group, Post, User
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
-
-
-from posts.models import Comment, Post
+from rest_framework.validators import UniqueTogetherValidator
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    """Сериализатор для модели Post."""
+
+    author = SlugRelatedField(slug_field="username", read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = "__all__"
+        read_only_fields = (
+            "id",
+            "pub_date",
+        )
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+    """Сериализатор для модели Comment."""
+
+    author = SlugRelatedField(slug_field="username", read_only=True)
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = "__all__"
+        read_only_fields = (
+            "id",
+            "post",
+            "created",
+        )
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Group."""
+
+    class Meta:
+        model = Group
+        fields = "__all__"
+        read_only_fields = ("__all__",)
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Follow."""
+
+    user = serializers.SlugRelatedField(
+        slug_field="username",
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault(),
+    )
+    following = serializers.SlugRelatedField(
+        slug_field="username", queryset=User.objects.all()
     )
 
     class Meta:
-        fields = '__all__'
-        model = Comment
+        model = Follow
+        fields = "__all__"
+        validators = (
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=("user", "following"),
+                message=("Подписка уже существует!"),
+            ),
+        )
+
+    def validate(self, data):
+        if data["user"] == data["following"]:
+            raise serializers.ValidationError(
+                "Нельзя подписаться на самого себя!"
+            )
+        return data
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,26 +1,112 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+from .validators import validate_not_empty
+
 User = get_user_model()
 
 
-class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+class Group(models.Model):
+    title = models.CharField(
+        verbose_name="Названние группы",
+        max_length=200,
+        help_text="Максимальная длина – 200 символов",
+    )
+    slug = models.SlugField(
+        verbose_name="Уникальный адрес",
+        max_length=200,
+        unique=True,
+        help_text="Уникальное значение. Максимальная длина – 200 символов",
+    )
+    description = models.TextField(verbose_name="Описание группы")
+
+    class Meta:
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
 
     def __str__(self):
-        return self.text
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField(
+        verbose_name="Текст поста",
+        validators=[validate_not_empty],
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts",
+        verbose_name="Автор",
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="posts",
+        verbose_name="Группа",
+    )
+    image = models.ImageField("Картинка", upload_to="posts/", blank=True)
+    pub_date = models.DateTimeField(
+        "Дата публикации", auto_now_add=True, db_index=True
+    )
+
+    class Meta:
+        ordering = ("pub_date",)
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
+    def __str__(self):
+        return self.text[:15]
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
+        Post,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="comments",
+        verbose_name="Пост",
+        help_text="Комментарии к посту",
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name="Автор",
+    )
+    text = models.TextField(
+        verbose_name="Текст комментария",
+        validators=[validate_not_empty],
+        help_text="Введите текст комментария",
+    )
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        "Дата публикации", auto_now_add=True, db_index=True
+    )
+
+    class Meta:
+        ordering = ("-created",)
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["post", "author"], name="Уникальность подписки"
+            )
+        ]
+
+
+class Follow(models.Model):
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="following",
+        verbose_name="Автор",
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="follower",
+        verbose_name="Подписчик",
+    )
